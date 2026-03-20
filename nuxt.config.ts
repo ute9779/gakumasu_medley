46818b8fe06eaaa099e1ac653d20d60fd2bbde9c
ute9779 <ute9779@gmail.com>
2026-03-20 15:58:45 +0900

    サイト公開対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/gakumasu_medley/' 
+  },
+  ssr: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
